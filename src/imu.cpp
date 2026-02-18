d529d507d7b04d18c0d2551f599093991997a2a0
Yifei Yuan <setsunad@outlook.jp>
2026-02-18 01:28:47 +0000

    Successful IMU Init

--- src/imu.cpp
+++ src/imu.cpp
@@ -3,9 +3,32 @@
 #include <Wire.h>
 #include <ICM45686.h>
 
+static const uint16_t ACCEL_FSR_G = 16;
+static const uint16_t GYRO_FSR_DPS = 2000;
+static const uint16_t ODR_HZ = 1600;
+
+static const float G_MS2 = 9.80665f;
+static const float DEG_2_RAD = 3.14159265f / 180.0f;
+
+static const float ACCEL_SCALE = (float)ACCEL_FSR_G / 32768.0f * G_MS2;
+static const float GYRO_SCALE  = (float)GYRO_FSR_DPS / 32768.0f * DEG_2_RAD;
+
+static ICM456xx IMU(Wire, 0);
+
+static float gyro_bias[3] = {0.0f,0.0f,0.0f};
+
 bool imu_init() 
 {
-return true; // dummy
+    int ret = IMU.begin();
+    if (ret != 0) return false;
+
+    ret = IMU.startAccel(ODR_HZ, ACCEL_FSR_G);
+    if (ret != 0) return false;
+
+    ret = IMU.startGyro(ODR_HZ, GYRO_FSR_DPS);
+    if (ret != 0) return false;
+
+    return true;
 }
 
 bool imu_read(FltData_t* fltdata)
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 #include <Arduino.h>
-
+#include <Wire.h>
 #include "types.h"
 #include "imu.h"
 
@@ -8,6 +8,15 @@ FltData_t fltdata;  // Init shared flight data struct
 
 void setup() {
 
+  Wire.begin();
+  Wire.setClock(1000000);
+  
+  Serial.begin(0); // ACM doesnt need baud rate
+
+  while(!Serial.available()) delay(1);
+
+  int ret = imu_init();
+  Serial.println(ret ? "IMU Init success" : "fuck you");
 
 }
 
